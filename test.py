c124be492de8409d29984e0dded2da94b5544a0a
Moss Chen <69073652+Mossforest@users.noreply.github.com>
2022-03-26 13:03:45 +0800

    test imgs

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from logger import setup_logger
 from model import BiSeNet
 
 import torch
+import time
 
 import os
 import os.path as osp
@@ -14,37 +15,38 @@ import torchvision.transforms as transforms
 import cv2
 
 def vis_parsing_maps(im, parsing_anno, stride, save_im=False, save_path='vis_results/parsing_map_on_im.jpg'):
+    # 注释掉的部分与生成可视化label图片有关，在大批量生成时注释掉以提高效率与空间利用度
     # Colors for all 20 parts
-    part_colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0],
-                   [255, 0, 85], [255, 0, 170],
-                   [0, 255, 0], [85, 255, 0], [170, 255, 0],
-                   [0, 255, 85], [0, 255, 170],
-                   [0, 0, 255], [85, 0, 255], [170, 0, 255],
-                   [0, 85, 255], [0, 170, 255],
-                   [255, 255, 0], [255, 255, 85], [255, 255, 170],
-                   [255, 0, 255], [255, 85, 255], [255, 170, 255],
-                   [0, 255, 255], [85, 255, 255], [170, 255, 255]]
+    # part_colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0],
+    #                [255, 0, 85], [255, 0, 170],
+    #                [0, 255, 0], [85, 255, 0], [170, 255, 0],
+    #                [0, 255, 85], [0, 255, 170],
+    #                [0, 0, 255], [85, 0, 255], [170, 0, 255],
+    #                [0, 85, 255], [0, 170, 255],
+    #                [255, 255, 0], [255, 255, 85], [255, 255, 170],
+    #                [255, 0, 255], [255, 85, 255], [255, 170, 255],
+    #                [0, 255, 255], [85, 255, 255], [170, 255, 255]]
 
     im = np.array(im)
     vis_im = im.copy().astype(np.uint8)
     vis_parsing_anno = parsing_anno.copy().astype(np.uint8)
     vis_parsing_anno = cv2.resize(vis_parsing_anno, None, fx=stride, fy=stride, interpolation=cv2.INTER_NEAREST)
-    vis_parsing_anno_color = np.zeros((vis_parsing_anno.shape[0], vis_parsing_anno.shape[1], 3)) + 255
+    # vis_parsing_anno_color = np.zeros((vis_parsing_anno.shape[0], vis_parsing_anno.shape[1], 3)) + 255
+    #
+    # num_of_class = np.max(vis_parsing_anno)
+    #
+    # for pi in range(1, num_of_class + 1):
+    #     index = np.where(vis_parsing_anno == pi)
+    #     vis_parsing_anno_color[index[0], index[1], :] = part_colors[pi]
 
-    num_of_class = np.max(vis_parsing_anno)
-
-    for pi in range(1, num_of_class + 1):
-        index = np.where(vis_parsing_anno == pi)
-        vis_parsing_anno_color[index[0], index[1], :] = part_colors[pi]
-
-    vis_parsing_anno_color = vis_parsing_anno_color.astype(np.uint8)
+    # vis_parsing_anno_color = vis_parsing_anno_color.astype(np.uint8)
     # print(vis_parsing_anno_color.shape, vis_im.shape)
-    vis_im = cv2.addWeighted(cv2.cvtColor(vis_im, cv2.COLOR_RGB2BGR), 0.4, vis_parsing_anno_color, 0.6, 0)
+    # vis_im = cv2.addWeighted(cv2.cvtColor(vis_im, cv2.COLOR_RGB2BGR), 0.4, vis_parsing_anno_color, 0.6, 0)
 
     # Save result or not
     if save_im:
         cv2.imwrite(save_path[:-4] +'.png', vis_parsing_anno)
-        cv2.imwrite(save_path, vis_im, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+        # cv2.imwrite(save_path, vis_im, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
 
     # return vis_im
 
@@ -74,7 +76,7 @@ def evaluate(respth='./res/test_res', dspth='./data', cp='model_final_diss.pth')
             out = net(img)[0]
             parsing = out.squeeze(0).cpu().numpy().argmax(0)
             # print(parsing)
-            print(np.unique(parsing))
+            # print(np.unique(parsing))
 
             vis_parsing_maps(image, parsing, stride=1, save_im=True, save_path=osp.join(respth, image_path))
 
@@ -82,9 +84,10 @@ def evaluate(respth='./res/test_res', dspth='./data', cp='model_final_diss.pth')
 
 
 
-
-
 if __name__ == "__main__":
-    evaluate(dspth='/home/zll/data/CelebAMask-HQ/test-img', cp='79999_iter.pth')
+    start = time.time()
+    evaluate(dspth='./CelebAMask-HQ/CelebA-HQ-img', cp='79999_iter.pth')
+    end = time.time()
+    print("Total time: " + str(end - start))
 
 
